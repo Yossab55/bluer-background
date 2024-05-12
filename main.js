683db11f01feff6068441eb6bd48af9eb10d5f2d
Yossab_Samuel <yossabpro5@gmail.com>
2024-05-12 17:39:19 +0300

    adject animation

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 let image = document.getElementById("test");
 let bluerEffect = 100;
 image.style.filter = `blur(${bluerEffect}px)`;
-
 let percentageText = document.querySelector(".percent span");
-
-let percentageInterval = setInterval(function () {
-  image.style.filter = `blur(${--bluerEffect}px)`;
-  if (percentageText.innerHTML == 100) {
-    clearInterval(percentageInterval);
-    setTimeout(() => percentageText.parentElement.remove(), 500);
-    return;
-  }
-  percentageText.innerHTML++;
-}, 30);
+let seconds = 100;
+makeAnimation();
+function makeAnimation() {
+  let percentageTimeOut = setTimeout(function () {
+    image.style.filter = `blur(${--bluerEffect}px)`;
+    if (percentageText.innerHTML == 100) {
+      clearTimeout(percentageTimeOut);
+      setTimeout(() => percentageText.parentElement.remove(), 500);
+      return;
+    }
+    percentageText.innerHTML++;
+    seconds --;
+    makeAnimation();
+  }, seconds);
+}
